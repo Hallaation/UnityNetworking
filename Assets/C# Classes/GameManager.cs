35c4869732f9449d24f74155bfe32539d9e4ab38
Louis Nguyen <nguyenlouis4@gmail.com>
2017-11-23 17:24:11 +1100

    adding stuff?

---
+++ Assets/C# Classes/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour
+{
+    //Gamemanager to be used for server side checking
+    public List<PlayerController> mPLayerList;
+    // Use this for initialization
+    void Start()
+    {
+        this.enabled = isServer;
+        if (isServer)
+        {
+            mPLayerList = new List<PlayerController>(FindObjectsOfType<PlayerController>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < mPLayerList.Count; i++)
+        {
+            if (mPLayerList[i].Health < 0)
+            {
+                Debug.Log("Player dead");
+            }
+        }
+    }
+
+}
---
+++ Assets/C# Classes/PlayerMessageSender.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+public class PlayerMessageSender : NetworkBehaviour
+{
+    NetworkClient mClient;
+
+    void Start()
+    {
+        SetupClient();
+    }
+
+    public class MyMsgType
+    {
+        public static short Score = MsgType.Highest + 1; //the message ID
+    }
+
+    public class ScoreMessage : MessageBase
+    {
+        public int score;
+        public Vector3 scorePos;
+        public int lives;
+    }
+
+    public void SendScore(int score, Vector3 scorePos, int lives)
+    {
+        ScoreMessage msg = new ScoreMessage();
+        msg.score = score;
+        msg.scorePos = scorePos;
+        msg.lives = lives;
+        NetworkServer.SendToAll(MyMsgType.Score, msg);
+    }
+
+    public void SetupClient()
+    {
+        mClient = new NetworkClient();
+        mClient.RegisterHandler(MsgType.Connect, OnConnected);
+        mClient.RegisterHandler(MyMsgType.Score, OnScore);
+        //mClient.Connect("127.0.0.1", 7777);
+    }
+
+    public void OnScore(NetworkMessage netMsg)
+    {
+        ScoreMessage msg = netMsg.ReadMessage<ScoreMessage>(); //deserialize
+        Debug.Log("OnScoreMessage " + msg.score);
+    }
+
+    public void OnConnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Connected to server");
+    }
+}
