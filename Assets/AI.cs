accbfedb7ee93876f994e56adee3e1572a291bb5
Louis Nguyen <nguyenlouis4@gmail.com>
2017-11-24 17:25:13 +1100

    navmesh test

---
+++ Assets/AI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AI : MonoBehaviour
+{
+    NavMeshAgent mAgent;
+    
+    Transform mTransform;
+    Rigidbody mRigidBody;
+    // Use this for initialization
+    void Start()
+    {
+        mAgent = this.GetComponent<NavMeshAgent>();
+        mTransform = this.transform;
+        mRigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
